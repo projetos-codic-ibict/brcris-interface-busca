2eaabe36165ec3847a19f9f520ad685aaf8b394c
rafaelramos64 <rafaelmaggot64@gmail.com>
2022-11-02 23:56:43 -0300

    feat: Adicionar  teste para conversão de nodes

--- src/pages/testeVisNetwork.tsx
+++ src/pages/testeVisNetwork.tsx
@@ -7,7 +7,60 @@ import useVisNetwork from './useVisNetwork'
 
 import useSWR from 'swr'
 
-const nodes: Node[] = [
+
+const nodesTest: any[] = [
+  {
+    id: '1',
+    institutions: {
+      id: '1',
+      name: 'Fiocruz',
+    }
+  },
+  {
+    id: '2',
+    institutions: {
+      id: '2',
+      name: 'São Paulo',
+    }
+  },
+  {
+    id: '3',
+    institutions: {
+      id: '3',
+      name: 'Pernambuco',
+    }
+  },
+  {
+    id: '4',
+    institutions: {
+      id: '4',
+      name: 'Fiocruz',
+    }
+  },
+  {
+    id: '5',
+    institutions: {
+      id: '5',
+      name: 'São Paulo',
+    }
+  },
+  {
+    id: '6',
+    institutions: {
+      id: '6',
+      name: 'Pernambuco',
+    }
+  },
+  {
+    id: '7',
+    institutions: {
+      id: '7',
+      name: 'Pernambuco',
+    }
+  }
+]
+
+/* const nodes: Node[] = [
   {
     id: 1,
     label: 'um',
@@ -55,7 +108,18 @@ const nodes: Node[] = [
     // level: 3,
     group: 'object',
   },
-]
+] */
+
+let nodesConverted: any[] = []
+
+for (let article of nodesTest) {
+  nodesConverted.push({ id: parseFloat(article.institutions?.id), label: article.institutions?.name })
+}
+
+console.log('123', nodesConverted)
+
+const nodes: Node[] = nodesConverted
+
 
 const edges: Edge[] = [
   { from: 1, to: 2, id: 1 },
@@ -102,14 +166,14 @@ const options: Options = {
 }
 
 export default () => {
-  const fetcher = (...args: Array<any>) =>
+  /* const fetcher = (...args: Array<any>) =>
     fetch(...(args as [any])).then((res) => res.json())
 
   const { data } = useSWR('/api/visNetwork', fetcher)
   const articles = data
   let notas = []
 
-  if (articles !== undefined) {
+  if (articles !== undefined || articles !== null) {
     for (let article of articles) {
       for (let institution of article._source.institutions) {
         if (institution.id !== nodes[article._source.institutions[institution]]?.id) {
@@ -118,8 +182,8 @@ export default () => {
       }
     }
     //console.log('123', data[1]._source.institutions)
-    console.log('123', notas)
-  }
+    console.log('teste: ', notas)
+  } */
  
 
   const { ref, network } = useVisNetwork({
