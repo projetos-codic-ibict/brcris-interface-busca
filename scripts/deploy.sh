56940ac05d90f80c0cd422b7252b35545d2f3581
Jesiel Viana <jesielviana@proton.me>
2022-12-19 18:05:20 -0300

    deploy automatizado no teste

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Running npm install"
-npm install
+echo "Running yarn install"
+yarn 
 echo "Running pm2 stop brcris-nextjs"
 pm2 stop brcris-nextjs
-echo "npm run deploy"
-npm run deploy
+echo "yarn deploy"
+yarn deploy
 echo "finished!"
