003aee5173771327e7afd5a453ae8eecf3019a68
Jesiel Viana <jesielviana@proton.me>
2023-01-11 18:57:15 -0300

    atualização do script de deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,8 @@ echo "Running yarn install"
 yarn 
 echo "Running pm2 stop brcris-nextjs"
 pm2 stop brcris-nextjs
+echo "copy env"
+cp .env.example  .env
 echo "yarn deploy"
 yarn deploy
 echo "finished!"
