dc2b6b771e5fda9ea1db2f2b4772e738bef9c258
Jesiel Viana <jesielviana@proton.me>
2023-03-16 13:09:42 -0300

    add google tradutor

---
+++ src/components/GoogleTranslate.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+
+import styles from '../styles/Translate.module.css'
+
+const App = () => {
+  const [isActive, setActive] = useState(false)
+
+  const toggleClass = () => {
+    setActive(!isActive)
+  }
+
+  const googleTranslateElementInit = () => {
+    // @ts-ignore
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: 'pt-br',
+        autoDisplay: false,
+      },
+      'google_translate_element'
+    )
+  }
+
+  useEffect(() => {
+    const addNewScript = document.createElement('script')
+    addNewScript.setAttribute(
+      'src',
+      '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
+    )
+    document.body.appendChild(addNewScript)
+    // @ts-ignore
+    window.googleTranslateElementInit = googleTranslateElementInit
+  }, [])
+  return (
+    <>
+      <div
+        className={`${styles.translateBtn} ${isActive ? styles.active : ''}`}
+      >
+        <div>
+          <a
+            onClick={toggleClass}
+            className={styles.toggle}
+            data-tooltip="Traduzir página"
+          >
+            <Image
+              src="/translator_icon.png"
+              alt="Ícone do google tradutor"
+              width={32}
+              height={32}
+            />
+          </a>
+        </div>
+        <div id="google_translate_element"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
+import GoogleTranslate from '../components/GoogleTranslate'
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     require('bootstrap/dist/js/bootstrap.bundle.min.js')
   }, [])
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleTranslate />
+      <Component {...pageProps} />{' '}
+    </>
+  )
 }
 
 export default MyApp
