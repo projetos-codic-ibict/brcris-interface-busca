48be6f8bc972c40bd3a1e254ecf53efb0906c5b6
rafaelramos64 <rafaelmaggot64@gmail.com>
2023-05-11 01:45:50 -0300

    feat: adicionando states ao componente ContactForm

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,10 +3,28 @@ import 'bootstrap/dist/css/bootstrap.min.css' // Import bootstrap CSS
 import { useTranslation } from 'next-i18next'
 import contactForm from '../styles/ContactForm.module.css'
 /* import Link from 'next/link' */
+import { useState } from 'react'
 
 function ContactForm() {
   /* const router = useRouter() */
   const { t } = useTranslation('contactForm')
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  /* const [submitted, setSubmitted] = useState(false) */
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+
+    const data = {
+      name,
+      email,
+      message,
+    }
+    console.log('Enviando contato', data)
+  }
 
   return (
     <div className="card">
@@ -15,13 +33,16 @@ function ContactForm() {
           <h5 className="text-center">{t('ContactUs')}</h5>
         </div>
 
-        <form action="sendEmail()">
+        <form action="">
           <div className="col-sm-12">
             <input
               className="form-control seacrh-box"
               type="text"
               placeholder={`${t('Name')}`}
               required
+              onChange={(event) => {
+                setName(event.target.value)
+              }}
             />
           </div>
 
@@ -31,6 +52,9 @@ function ContactForm() {
               type="email"
               placeholder={`${t('Email')}`}
               required
+              onChange={(event) => {
+                setEmail(event.target.value)
+              }}
             />
           </div>
 
@@ -40,11 +64,20 @@ function ContactForm() {
               type="text"
               placeholder={`${t('Message')}`}
               required
+              onChange={(event) => {
+                setMessage(event.target.value)
+              }}
             />
           </div>
 
           <div className="submit-btn col-sm-12 mt-2 d-flex justify-content-end">
-            <button className="btn btn-light" type="submit">
+            <button
+              className="btn btn-light"
+              type="submit"
+              onChange={(event) => {
+                handleSubmit(event)
+              }}
+            >
               {t('Submit')}
             </button>
           </div>
