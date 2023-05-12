696ef1ce33129eda57bb59a4efccde7cd1568533
Jesiel Viana <jesielviana@proton.me>
2023-05-12 08:02:15 -0300

    corrigindo tradução do form de contato

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,3 @@
-import { useTranslation } from 'next-i18next'
-
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { GetStaticProps } from 'next'
 import Navbar from '../components/Navbar'
@@ -11,12 +9,11 @@ type Props = {
 // or getServerSideProps: GetServerSideProps<Props> = async ({ locale })
 export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => ({
   props: {
-    ...(await serverSideTranslations(locale ?? 'en', ['common', 'navbar'])),
+    ...(await serverSideTranslations(locale ?? 'en', ['contact', 'navbar'])),
   },
 })
 
 export default function About() {
-  const { t } = useTranslation('contact')
   return (
     <>
       <Navbar />
@@ -24,14 +21,6 @@ export default function About() {
       <div className="page-search">
         <div className="App">
           <div className="container page">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="page-title">
-                  <h2>{t('Contact')}</h2>
-                </div>
-              </div>
-            </div>
-
             <div className="row justify-content-center m-5">
               <div className="col-md-8">
                 <ContactForm />
