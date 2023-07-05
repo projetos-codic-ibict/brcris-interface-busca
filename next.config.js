c7c665d995e1dd408de40480b7497c793d25aeef
Jesiel Viana <jesielviana@proton.me>
2023-07-05 17:13:02 -0300

    add configuração de proxy para APIs

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/:path*', // Proxy to Backend
+        destination: 'http://localhost:3000/:path*', // Proxy to Backend
       },
     ];
   },
