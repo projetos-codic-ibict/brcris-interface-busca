418ece7b9f1c6dcfef5c39e31d20ff0375d4ebe3
Jesiel Viana <jesielviana@proton.me>
2023-11-03 13:31:10 -0300

    voltando para o yarn

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Running NPM install"
-npm ci 
+echo "Running yarn install"
+yarn 
 echo "Running pm2 stop brcris-nextjs"
 pm2 stop brcris-nextjs
 echo "copy env"
 cp .env.example  .env
-echo "NPM deploy"
-npm run deploy
+echo "yarn deploy"
+yarn deploy
 echo "finished!"
