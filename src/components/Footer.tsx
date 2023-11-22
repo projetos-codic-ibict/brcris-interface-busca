4ef45b0fc771a45e127124c81e149a760d598140
Jesiel Viana <jesielviana@proton.me>
2023-11-22 13:14:28 -0300

    add linkedin na barra de redes sociais

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,36 +2,16 @@ function Footer() {
   return (
     <footer role="contentinfo">
       <div className="social-media-icons">
-        <a
-          href="https://www.youtube.com/user/IBICTbr/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+        <a href="https://www.youtube.com/user/IBICTbr/" target="_blank" rel="noreferrer">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M21.5821 7.16905C21.3521 6.30296 20.6744 5.62086 19.8139 5.38938C18.2542 4.96875 12 4.96875 12 4.96875C12 4.96875 5.74586 4.96875 4.18613 5.38938C3.32565 5.6209 2.64794 6.30296 2.41792 7.16905C2 8.73889 2 12.0142 2 12.0142C2 12.0142 2 15.2895 2.41792 16.8593C2.64794 17.7254 3.32565 18.3791 4.18613 18.6106C5.74586 19.0312 12 19.0312 12 19.0312C12 19.0312 18.2541 19.0312 19.8139 18.6106C20.6744 18.3791 21.3521 17.7254 21.5821 16.8593C22 15.2895 22 12.0142 22 12.0142C22 12.0142 22 8.73889 21.5821 7.16905V7.16905ZM9.95453 14.9879V9.04046L15.1818 12.0143L9.95453 14.9879V14.9879Z"
               fill="white"
             ></path>
           </svg>
         </a>
-        <a
-          href="https://www.facebook.com/IBICTbr/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+        <a href="https://www.facebook.com/IBICTbr/" target="_blank" rel="noreferrer">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M16.6539 13.25L17.2094 9.63047H13.7364V7.28164C13.7364 6.29141 14.2215 5.32617 15.777 5.32617H17.3559V2.24453C17.3559 2.24453 15.9231 2 14.5531 2C11.693 2 9.82346 3.73359 9.82346 6.87188V9.63047H6.64417V13.25H9.82346V22H13.7364V13.25H16.6539Z"
               fill="white"
@@ -39,33 +19,34 @@ function Footer() {
           </svg>
         </a>
         <a href="https://twitter.com/ibictbr" target="_blank" rel="noreferrer">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M19.9442 7.92639C19.9569 8.10405 19.9569 8.28175 19.9569 8.4594C19.9569 13.8782 15.8326 20.1219 8.29444 20.1219C5.97209 20.1219 3.81473 19.4492 2 18.2818C2.32996 18.3198 2.64719 18.3325 2.98985 18.3325C4.90607 18.3325 6.67006 17.6853 8.0787 16.5813C6.27666 16.5432 4.7665 15.363 4.24618 13.7386C4.50001 13.7766 4.7538 13.802 5.02032 13.802C5.38833 13.802 5.75638 13.7513 6.099 13.6625C4.22083 13.2817 2.81215 11.632 2.81215 9.6396V9.58886C3.35782 9.89343 3.99239 10.0838 4.66493 10.1091C3.56087 9.37308 2.83754 8.11675 2.83754 6.69541C2.83754 5.934 3.04055 5.23603 3.3959 4.62689C5.41369 7.1142 8.44671 8.73854 11.8477 8.91624C11.7843 8.61167 11.7462 8.29444 11.7462 7.97717C11.7462 5.71826 13.5736 3.87817 15.8452 3.87817C17.0254 3.87817 18.0914 4.3731 18.8401 5.17259C19.7665 4.99494 20.6548 4.65228 21.4416 4.18275C21.137 5.13455 20.4898 5.93404 19.6396 6.44162C20.4645 6.35283 21.264 6.12435 22 5.80713C21.4417 6.61928 20.7437 7.3426 19.9442 7.92639V7.92639Z"
               fill="white"
             ></path>
           </svg>
         </a>
+        <a
+          href="https://www.linkedin.com/company/ibict---instituto-brasileiro-de-informa-o-em-ci-ncia-e-tecnologia/mycompany/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg width="18" height="18" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="m13.6313333 13.6346667h-2.3693333v-3.7126667c0-.88533333-.018-2.02466667-1.2346667-2.02466667-1.2353333 0-1.42399997.96333334-1.42399997 1.95933334v3.77800003h-2.36933333v-7.6346667h2.276v1.04066667h.03066667c.318-.6 1.09133333-1.23333334 2.24666663-1.23333334 2.4006667 0 2.8446667 1.58 2.8446667 3.63666667v4.1906667zm-10.0733333-8.67933337c-.76266667 0-1.37533333-.61733333-1.37533333-1.37666666 0-.75866667.61333333-1.37533334 1.37533333-1.37533334.76 0 1.376.61666667 1.376 1.37533334 0 .75933333-.61666667 1.37666666-1.376 1.37666666zm1.188 8.67933337h-2.376v-7.6346667h2.376zm10.0706667-13.6346667h-13.63600003c-.65266667 0-1.18066667.516-1.18066667 1.15266667v13.69466663c0 .6373334.528 1.1526667 1.18066667 1.1526667h13.63400003c.652 0 1.1853333-.5153333 1.1853333-1.1526667v-13.69466663c0-.63666667-.5333333-1.15266667-1.1853333-1.15266667z"
+              fill="#fff"
+            />
+          </svg>
+        </a>
       </div>
       <div className="footer">
         <picture>
-          <img
-            src="/logos/footer-logo.png"
-            className="logo-rodape"
-            alt="imagem com logo do ibict, minc e gov.br"
-          />
+          <img src="/logos/footer-logo.png" className="logo-rodape" alt="imagem com logo do ibict, minc e gov.br" />
         </picture>
         <p className="footer-adress">
           Instituto Brasileiro de Informação em Ciência e Tecnologia (Ibict)
           <br />
-          SAUS Quadra 5 - Lote 6 Bloco H - Asa sul - CEP: 70.070-912 - Brasília
-          - DF
+          SAUS Quadra 5 - Lote 6 Bloco H - Asa sul - CEP: 70.070-912 - Brasília - DF
         </p>
       </div>
     </footer>
