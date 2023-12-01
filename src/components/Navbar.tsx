20c165d9167c4bf50f5ba94cd729d2f7f45abab1
Jesiel Viana <jesielviana@proton.me>
2023-12-01 11:38:41 -0300

    add menu de tools: visão e carrot2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,6 +54,40 @@ function Navbar() {
                 <a className="nav-link">{t('Dashboards')}</a>
               </Link>
             </li>
+            <li className="nav-item me-5" role="presentation">
+              <div className={dropdownStyle.dropdown}>
+                <div className={dropdownStyle.flexCenter}>
+                  <a href="#" className="nav-link">
+                    {t('Tools')}
+                    <svg
+                      height="20"
+                      width="20"
+                      viewBox="0 0 20 20"
+                      aria-hidden="true"
+                      focusable="false"
+                      className="css-tj5bde-Svg"
+                    >
+                      <path
+                        fill="#fff"
+                        d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+                <div className={dropdownStyle.dropdownContent}>
+                  <a href="http://carrot.ibict.br/frontend/#/workbench" target="_blank" rel="noopener noreferrer">
+                    {t('Carrot2')}
+                  </a>
+                  <a
+                    href="https://visao.ibict.br/visao2/viewGroupCategory/403"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {t('Vision')}
+                  </a>
+                </div>
+              </div>
+            </li>
 
             <li className="nav-item me-5" role="presentation">
               <Link href="/team">
