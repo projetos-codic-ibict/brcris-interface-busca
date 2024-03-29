a51579b2ae4efface8b8fc91b67085654e41d125
Jesiel Viana <jesielviana@proton.me>
2024-03-29 09:53:30 -0300

    loggin env

--- src/services/Logger.ts
+++ src/services/Logger.ts
@@ -20,4 +20,10 @@ const fileTransport = pino.transport({
 
 const logger = pino(config, fileTransport);
 
+logger.info('API_KEY:', process.env.API_KEY);
+logger.info('MAIL_SENDER:', process.env.MAIL_SENDER);
+logger.info('MAIL_PASSWORD:', process.env.MAIL_PASSWORD);
+logger.info('MAIL_HOST:', process.env.MAIL_HOST);
+logger.info('RECAPTCHA_SECRET_KEY:', process.env.RECAPTCHA_SECRET_KEY);
+
 export default logger;
