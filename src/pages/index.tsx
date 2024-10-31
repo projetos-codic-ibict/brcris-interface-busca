a0d875e76cfc9b765efe62ef220b6c47369b0e87
Jesiel Viana <jesielviana@proton.me>
2024-10-31 16:37:22 -0300

    alterando o layout da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
-import { IoSearch } from 'react-icons/io5';
+import { IoArrowDown, IoSearch } from 'react-icons/io5';
 import styles from '../styles/Home.module.css';
 
 type Props = {
@@ -111,7 +111,7 @@ export default function App() {
         <div className="search-card">
           <h1>{t('Search in the Brazilian Scientific Research Information Ecosystem')} (BrCris)</h1>
           <form className="form-search" action={`/${router.locale}/${searchPage}`}>
-            <div>
+            <div className="custom-select">
               <select id="index-select" onChange={handleSelectChange} title={t('Select an entity') || ''}>
                 {indexes.map((index) => (
                   <option key={index.name} value={index.name}>
@@ -119,40 +119,68 @@ export default function App() {
                   </option>
                 ))}
               </select>
-              <input
-                ref={inputRef}
-                name="q"
-                autoFocus
-                title={`${t('Enter at least 3 characters and search among')} ${t('numberFormat', {
-                  value: docsCount,
-                })} ${t(searchPage)}`}
-                type="text"
-                value={term}
-                onChange={(e) => setTerm(e.target.value)}
-                placeholder={`${t('Enter at least 3 characters and search among')} ${t('numberFormat', {
-                  value: docsCount,
-                })} ${t(searchPage)}`}
-              />
             </div>
+            <input
+              ref={inputRef}
+              name="q"
+              autoFocus
+              title={`${t('Enter at least 3 characters and search among')} ${t('numberFormat', {
+                value: docsCount,
+              })} ${t(searchPage)}`}
+              type="text"
+              value={term}
+              onChange={(e) => setTerm(e.target.value)}
+              placeholder={`${t('Enter at least 3 characters and search among')} ${t('numberFormat', {
+                value: docsCount,
+              })} ${t(searchPage)}`}
+            />
             <button disabled={term?.trim().length < 3} className="btn btn-primary" title={t('Search') || 'Search'}>
               <IoSearch /> {t('Search')}
             </button>
           </form>
         </div>
       </div>
-      <section id="partners" className="container-fluid">
-        <h2 className="text-center">{t('BrCris Partners')}</h2>
-        <div className="partners">
-          {partners.map((partner, index) => (
-            <div key={index} className={partner.class}>
-              <a href={partner.url} target="_blank" rel="noreferrer">
-                <picture>
-                  <img src={partner.path} alt={partner.description} />
-                </picture>
-              </a>
-            </div>
-          ))}
+      <section className={styles.informations}>
+        <div className={styles.more}>
+          <a href="#more">
+            <IoArrowDown />
+            Saiba mais
+          </a>
         </div>
+        <section className={styles.about}>
+          <div className="card text-center p-2">
+            <h2>{t('Open Science')}</h2>
+            <div className="card-body">
+              <p className="card-text">{t('lorem ipsum')}</p>
+            </div>
+          </div>
+          <div className="card text-center p-2">
+            <h2>{t('Data Sources')}</h2>
+            <div className="card-body">
+              <p className="card-text">{t('lorem ipsum')}</p>
+            </div>
+          </div>
+          <div className="card text-center p-2">
+            <h2>{t('About')}</h2>
+            <div className="card-body">
+              <p className="card-text">{t('lorem ipsum')}</p>
+            </div>
+          </div>
+        </section>
+        <section id="partners" className="container-fluid">
+          <h2 className="text-center">{t('BrCris Partners')}</h2>
+          <div className="partners">
+            {partners.map((partner, index) => (
+              <div key={index} className={partner.class}>
+                <a href={partner.url} target="_blank" rel="noreferrer">
+                  <picture>
+                    <img src={partner.path} alt={partner.description} />
+                  </picture>
+                </a>
+              </div>
+            ))}
+          </div>
+        </section>
       </section>
     </>
   );
