390dd4274386c7bf9740f822fe74bb0feac9b363
Jesiel Viana <jesielviana@proton.me>
2025-04-04 10:56:24 -0300

    atualização das publicações

--- src/components/about/Publications.tsx
+++ src/components/about/Publications.tsx
@@ -5,25 +5,31 @@ const Publications = () => {
     <>
       <h3>2024</h3>
       <Publication
+        authors="SOUZA; MARCEL; et al."
+        title="BrCris: A Catalyst for Advancing Open Science and Scientific Data Processing in Brazil"
+        link="http://hdl.handle.net/11366/2551"
+        journal="[Anais...]: XVI euroCRIS, 2024."
+      />
+      <Publication
         authors="SEGUNDO; W. L. R. C.; et al."
         title="Inovação e conectividade: uma perspectiva sobre o projeto BrCris e suas tecnologias para tratamento de dados científicos"
         link="https://www.brapci.inf.br/v/301399"
-        journal="In: Workshop de Informação Dados e Tecnologia, 2024., 2024. VII."
+        journal="[Anais...]: VII Workshop de Informação Dados e Tecnologia, 2024."
       />
       <Publication
         authors="DIAS; T. M. R.; et al."
         title="Análises métricas potencializadas: a contribuição da plataforma BrCris na integração de repositórios"
         link="https://www.brapci.inf.br/v/306005"
-        journal="In: Encontro Brasileiro de Bibliometria e Cientometria, 2024., 2024. IX."
+        journal="[Anais...]: IX Encontro Brasileiro de Bibliometria e Cientometria, 2024."
       />
       <Publication
         authors="DIAS; T. M. R.; et al."
         title="Como os dados exportados pela plataforma BrCris impulsionam análises bibliométricas e cientométricas"
         link="https://hdl.handle.net/20.500.11959/brapci/306003"
-        journal="In: Encontro Brasileiro de Bibliometria e Cientometria, 2024., 2024. IX."
+        journal="[Anais...]: IX Encontro Brasileiro de Bibliometria e Cientometria, 2024."
       />
       <Publication
-        authors="DIAS; T. M. R.; JUNIOR; R. F. G.; SILVA; V. S.; SOUZA; M. G.; OLIVEIRA; A. F.; CARVALHO-SEGUNDO; W"
+        authors="DIAS; T. M. R.; GABRIEL JUNIOR; R. F.; SILVA; V. S.; SOUZA; M. G.; OLIVEIRA; A. F.; CARVALHO-SEGUNDO; W"
         title="Development and Implementation of the BrCris and BrCris-T Platform: Integration of Scientific and Technological Data in Brazil"
         link="https://doi.org/10.1016/j.procs.2024.11.050"
         journal="Procedia Computer Science, v. 249, p. 66-77, 2024."
@@ -40,13 +46,13 @@ const Publications = () => {
         authors="VIDAL; L. H. C.; et al."
         title="BrCris: visualização de indicadores científicos"
         link="https://www.brapci.inf.br/v/257789"
-        journal="In: Encontro Nacional de Pesquisa e Pós-graduação em Ciência da Informação, 2023. XXIII."
+        journal="[Anais...]: XXIII Encontro Nacional de Pesquisa e Pós-graduação em Ciência da Informação, 2023."
       />
       <Publication
         authors="DIAS; T. M. R.; et al."
         title="Plataforma BrCris: uma abordagem abrangente para a análise e compreensão do ecossistema de pesquisa científica no Brasil"
         link="https://www.brapci.inf.br/v/257772"
-        journal="In: Encontro Nacional de Pesquisa e Pós-graduação em Ciência da Informação, 2023., 2023. XXIII."
+        journal="[Anais...]: XXIII Encontro Nacional de Pesquisa e Pós-graduação em Ciência da Informação, 2023."
       />
 
       <h3>2022</h3>
@@ -78,13 +84,13 @@ const Publications = () => {
         authors="DIAS; T. M. R.; MENA-CHALCO; J. P.; CARVALHO SEGUNDO; W. L. R.; PINTO; A. L.; QUONIAM; L.; MOREIRA; T. H. J.; SILVA; V. D. S"
         title="O Projeto BrCris: uma plataforma computacional para integração, visualização e prospecção de dados científicos"
         link="https://brapci.inf.br/v/199466"
-        journal="In: Encontro Brasileiro de Bibliometria e Cientometria, 2022. 8, p. 390-397."
+        journal="[Anais...]: 8 Encontro Brasileiro de Bibliometria e Cientometria, 2022, p. 390-397."
       />
       <Publication
         authors="DIAS; T. M. R.; CARVALHO SEGUNDO; W. L. R.; MOREIRA; T. H. J.; SILVA; V. S.; PINTO; A. L"
         title="Plataforma para agregação e análises de dados técnicos-científicos"
         link="https://widat2022.ufes.br/wp-content/uploads/2023/04/artigos/PLATAFORMA%20PARA%20AGREGA%C3%87%C3%83O%20E%20AN%C3%81LISES%20DE%20DADOS%20T%C3%89CNICOS-CIENT%C3%8DFICOS.pdf"
-        journal="In: Workshop de informação, dados e tecnologia, UFES, Vitória, 2022. V."
+        journal="[Anais...]: V Workshop de informação, dados e tecnologia, UFES, Vitória, 2022."
       />
 
       <h3>2021</h3>
@@ -92,7 +98,7 @@ const Publications = () => {
         authors="CARVALHO SEGUNDO; W.; DIAS; T. M. R.; MOREIRA; T.; PINTO; A. L.; SILVA; V. S.; GOMES; J.; QUONIAM; L.; MATAS; L.; DIAS; A. G.; SCHNEIDER; J"
         title="O BrCris como ferramenta de apoio à Ciência Aberta"
         link="https://zenodo.org/record/5576817"
-        journal="In: Conferência Luso-Brasilera de Ciência Aberta, 2021. XII, p. 1-37."
+        journal="[Anais...]: XII Conferência Luso-Brasilera de Ciência Aberta, 2021, p. 1-37."
       />
       <Publication
         authors="PINTO; A. L.; CARVALHO SEGUNDO; W. L. R.; QUONIAM; L.; DIAS; T. M. R"
@@ -104,7 +110,7 @@ const Publications = () => {
         authors="SILVA; V. S.; MOREIRA; T.; DIAS; T. M.; DIAS; J. G.; CARVALHO SEGUNDO; W. L. R"
         title="Um modelo semântico baseado em ontologia para o cris brasileiro."
         link="https://dialnet.unirioja.es/servlet/articulo?codigo=8411201#:~:text=Um%20modelo%20sem%C3%A2ntico%20baseado%20em%20ontologia%20para%20o%20cris%20brasileiro,-Autores%3A%20Vivian%20S&text=Objetivos%3A%20Elabora%C3%A7%C3%A3o%20de%20uma%20representa%C3%A7%C3%A3o,de%20pesquisa%20cient%C3%ADfica%20no%20Brasil"
-        journal="In: , 2021. ."
+        journal="[Anais...]:  , 2021."
       />
       <Publication
         authors="DIAS; T. M. R.; PINTO; A. L.; MENA-CHALCO; J. P.; CARVALHO SEGUNDO; W. L. R.; GOMES; J. C.; SILVA; R. R.; QUONIAM; L"
@@ -116,19 +122,19 @@ const Publications = () => {
         authors="CARVALHO SEGUNDO; W. L. R.; RODRIGUES; T. M"
         title="On building a tool for finding datasets based on a list of researchers or publications"
         link="https://zenodo.org/record/4506826"
-        journal="In: International Conference on Open Repositories, Denver, Colorado, 2021. XVI."
+        journal="[Anais...]: XVI International Conference on Open Repositories, Denver, Colorado, 2021."
       />
       <Publication
         authors="PINTO; A. L.; DIAS; T. M. R.; SILVA; V. S.; GOMES; J. C.; CARVALHO SEGUNDO; W. L. R"
         title="BrCris como um sistema de recomendação científico-tecnológica"
         link="https://www.researchgate.net/publication/356640074_BrCRIS_COMO_UM_SISTEMA_DE_RECOMENDACAO_CIENTIFICO-TECNOLOGICA"
-        journal="In: Encontro Nacional de Pesquisa e Pós-Graduação em Ciência da Informação, Rio de Janeiro, Brasil, 2021. XXI."
+        journal="[Anais...]: XXI Encontro Nacional de Pesquisa e Pós-Graduação em Ciência da Informação, Rio de Janeiro, Brasil, 2021."
       />
       <Publication
         authors="PINTO; A. L.; DIAS; T.; GOUVEIA; F. C.; CARVALHO SEGUNDO; W. R"
         title="Métricas de avaliação sobre revistas científicas para uso no BrCris/IBICT: um ensaio com dados da WoS e Google Scholar"
         link="https://www.researchgate.net/publication/354616911_Metricas_de_avaliacao_sobre_revistas_cientificas_para_uso_no_BrCrisIBICT_um_ensaio_com_dados_da_WoS_e_Google_Scholar"
-        journal="In: LATmetricas, Medelin, 2021. ."
+        journal="[Anais...]:  LATmetricas, Medelin, 2021."
       />
       <Publication
         authors="PINTO; A. L.; DIAS; T. M. R.; MOREIRA; T. H. J.; CARVALHO SEGUNDO; W. R"
@@ -140,7 +146,7 @@ const Publications = () => {
         authors="CARVALHO SEGUNDO; W. R.; DIAS; T.; MOREIRA; T.; PINTO; A.; SILVA; V.; GOMES; J.; QUONIAM; L.; MATAS; L.; DIAS; A.; SCHNEIDER; J"
         title="Uma Plataforma para o Tratamento e Integração de Dados Científicos"
         link="https://sol.sbc.org.br/index.php/latinoware/article/view/19913"
-        journal="In: Congresso Latino-Americano de Software Livre e Tecnologias Abertas, Foz do Iguaçu, Paraná., 2021. XVIII."
+        journal="[Anais...]: XVIII Congresso Latino-Americano de Software Livre e Tecnologias Abertas, Foz do Iguaçu, Paraná., 2021."
       />
 
       <h3>2019</h3>
@@ -154,7 +160,7 @@ const Publications = () => {
         authors="MATAS; L. J.; DIAS; T. M. R.; CARVALHO-SEGUNDO; W. L. R"
         title="Improving LA Referencia metadata by linking research profiles to repositories: the case of the Brazilian Digital Library of Thesis and Dissertations (BDTD) and the Lattes CV Platform"
         link="https://ridi.ibict.br/bitstream/123456789/1025/1/PaperLTW.pdf"
-        journal="In: Open Repositories, Hamburg, Germany., 2019. ."
+        journal="[Anais...]:  Open Repositories, Hamburg, Germany., 2019."
       />
     </>
   );
